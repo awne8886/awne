465b3e44815b8bdf0d3ec07aa1c0734eadd4aac5
awne8886 <131464649+awne8886@users.noreply.github.com>
2023-11-13 13:51:13 +0530

    Add files via upload

--- parrot/server.js
+++ parrot/server.js
@@ -2,7 +2,28 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.get('/parrot', (req, res) => {
+  res.send(getAnimatedParrotFrames());
+});
+
+function getAnimatedParrotFrames() {
+  const frames = [
+    // Add your ASCII art frames here (replace with actual frames)
+    'parrot\frame-000.ansi',
+    'parrot\frame-001.ansi',
+    'parrot\frame-002.ansi',
+    'parrot\frame-003.ansi',
+    'parrot\frame-004.ansi',
+    'parrot\frame-005.ansi',
+    'parrot\frame-006.ansi',
+    'parrot\frame-007.ansi',
+    'parrot\frame-008.ansi',
+    'parrot\frame-009.ansi',
+    // ...
+  ];
+
+  return frames.join('\n\n'); // Frames separated by two newlines for better visibility
+}
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
